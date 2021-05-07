7655b57e5f3aa08d1f35c0dc50d41ed2bc0bbc5b
Kri0k <68000966+Kri0k@users.noreply.github.com>
2021-05-07 19:34:11 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,75 @@
-let body = document.body;
-let string = window.location.search;
-let url = 'https://api.github.com/users/kri0k';
+  
+let date = new Date();
+let url = 'Kriok';
 
-fetch(url)
-    .then(res => res.json())
-    .then(json => {
-        console.log(json.avatar_url);
-        console.log(json.nick);
-        console.log(json.bio);
-        console.log(json.html_url);
+const getNameFromUrl = (url) => {
+  let getUrl = url.split('=');
+  let name = getUrl[1]; //
+  if(name == undefined) {
+    name = 'Kri0k';
+  }
+  return name;
+}
 
-        let photo = new Image();
-        photo.src = json.avatar_url;
-        body.append(photo);
+let userName = getNameFromUrl(url);
 
-        let name = document.createElement('p');
-        if (json.name != null) {
-            name.innerHTML = json.name;
-        } else {
-            name.innerHTML = 'Неизвестно';
-        }
-        body.append(nick);
+const getUser = new Promise((resolve,reject) => {
+  setTimeout(() => userName ? resolve(userName) : reject(alert('Ошибка с именем')), 2000);
+})
 
-        let bio = document.createElement('p');
-        if (json.bio != null) {
-            bio.innerHTML = json.bio;
-        } else {
-            bio.innerHTML = 'Неизвестно';
-        }
-        body.append(bio);
+const getDate = new Promise((resolve,reject) => {
+  setTimeout(() => date ? resolve(date) : reject('Ошибка с датой'), 4000);
+})
 
-    })
+Promise.all([getUser, getDate])
+.then(([name, date]) => fetch(`https://api.github.com/users/${userName}`))
+  .then((result) => {
+    let preloader = document.getElementById("preloader");
+    if (result.ok) {
+      return result.json();
+    } else {
+      loading = false;
+      alert('Информация о пользователе недоступна');
+      let error = new Error(result.statusText);
+      error.response = result;
+      throw error;
+    }
+    document.body.classList.remove('loading');
+  })
+  .then(json => {
+    console.log(json);
+
+    preloader.classList.add('loaded');
+
+    let img = new Image();
+    img.src = json.avatar_url;
+    img.className = 'img';
+    document.body.append(img);
+
+    let wrap = document.createElement('div');
+    wrap.className='wrap';
+    let name = document.createElement('a');
+    name.className = 'name';
+    name.href = `https://https://github.com/?username=${json.login}`;
+    name.innerHTML = json.name;
+    document.body.append(wrap);
+    if (json.name != null) {
+      document.querySelector('.wrap').append(name);
+    } else {
+      document.querySelector('.wrap').append('У пользователя нет имени!');
+    }
+
+    let bio = document.createElement('div');
+    bio.className = 'bio';
+    bio.innerHTML = json.bio;
+    if (json.bio != null) {
+      document.body.append(bio);
+    } else {
+      document.body.append('Нет информации о пользователе');
+    }
+
+    let currentDate = document.createElement('footer');
+    currentDate.innerHTML = `На дату: ${date.toLocaleDateString()}`;
+    document.body.append(currentDate);
+  })
+  .catch(error => console.log(error));
